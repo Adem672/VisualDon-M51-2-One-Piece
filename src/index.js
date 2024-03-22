b477cb334e26cfe317229ed1a14c330e628f92cf
Michael Cheneval <michael@cheneval.com>
2024-03-22 21:54:05 +0100

    Added backgrounds, scroll-snapping and some json data

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import data from "../data/one-piece.json";
+import data from "../data/saga-data.json";
 
 // Charger le fichier JSON (remplacer "chemin/vers/votre/fichier.json" par le chemin réel de votre fichier JSON)
 document.addEventListener("DOMContentLoaded", () => {
@@ -7,29 +7,62 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // Fonction pour afficher les données dans le HTML
 function afficherDonnees(data) {
-    // Récupérer l'élément HTML où vous souhaitez afficher les données (par exemple, un div avec id="conteneur")
-    const conteneur = document.getElementById('conteneur');
+    // Page de titre de la saga
+    const sagaTitle = data.Saga.Nom;
+    document.getElementById("title").innerHTML = `One Piece<br>${sagaTitle} Saga`;
 
-    // Parcourir les données JSON pour afficher chaque élément
-    for (const arc in data.Arcs) {
-        const arcData = data.Arcs[arc];
+    // Récupérer l'élément HTML où vous souhaitez afficher les données
+    const conteneur = document.querySelector('.container');
+
+    // Parcourir les données JSON pour afficher chaque arc
+    for (const arc in data.Saga.Arcs) {
+        // Containers
+        const arcData = data.Saga.Arcs[arc];
         const arcDiv = document.createElement('div');
+        arcDiv.classList.add("arc-background");
+        const dataDiv = document.createElement("div");
+        dataDiv.classList.add("arc-data");
 
+        // Arc title
         const title = document.createElement("h2");
         title.textContent = arcData.Nom;
 
+        // Arc protagonists
         const equipage = document.createElement("p");
-        equipage.textContent = `Membres de l'équipage : ${arcData.Membres_Chapeau_de_Paille.join(', ')}`;
+        equipage.textContent = "Membres de l'équipage :";
+        const membresAllies = document.createElement("ul");
+        arcData.Membres_Chapeau_de_Paille.forEach(membre => {
+            const nomMembre = document.createElement("li");
+            nomMembre.textContent = membre;
+            membresAllies.appendChild(nomMembre);
+        })
 
+        // Arc antagonists
         const ennemis = document.createElement("p");
-        equipage.textContent = `Personnes affrontées : ${arcData.Personnes_affrontees.join(', ')}`;
+        ennemis.textContent = "Personnes affrontées :";
+        const membresEnnemis = document.createElement("ul");
+        arcData.Personnes_affrontees.forEach(membre => {
+            const nomMembre = document.createElement("li");
+            nomMembre.textContent = membre;
+            membresEnnemis.appendChild(nomMembre);
+        })
 
-        const hr = document.createElement("hr");
+        // Arc duration
+        const chapitres = document.createElement("p");
+        chapitres.textContent = `Chapitres : ${arcData.Chapitres}`;
         
-        arcDiv.appendChild(title);
-        arcDiv.appendChild(equipage);
-        arcDiv.appendChild(ennemis);
-        arcDiv.appendChild(hr);
+        const episodes = document.createElement("p");
+        episodes.textContent = `Episodes : ${arcData.Episodes}`;
+        
+        // Append to containers
+        dataDiv.appendChild(title);
+        equipage.appendChild(membresAllies);
+        dataDiv.appendChild(equipage);
+        ennemis.appendChild(membresEnnemis);
+        dataDiv.appendChild(ennemis);
+        dataDiv.appendChild(chapitres);
+        dataDiv.appendChild(episodes);
+        arcDiv.appendChild(dataDiv);
         conteneur.appendChild(arcDiv);
     }
 }

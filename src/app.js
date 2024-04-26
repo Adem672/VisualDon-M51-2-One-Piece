44ca1a190f25f9193b22c7fcaa20c39290e540ed
Adem Ahmetovic <ademahmetovic2002@gmail.com>
2024-04-26 09:44:56 +0200

    repair this parcel bs

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import arcPage from './index_original.html'
 const images = [...document.querySelectorAll(".img")];
 const items = [...document.querySelectorAll(".item")];
 const slider = document.querySelector(".slider");
+const conteneur = document.querySelector('.container');
 const arcKeys = Object.keys(saga.Saga.Arcs);
 let counter = 0;
 let sliderWidth;
@@ -58,6 +59,90 @@ function animateImage() {
     })
 }
 
+function afficherDonnees() {
+    // Saga title
+    const sagaTitle = saga.Saga.Nom;
+    document.getElementById("title").innerHTML = `One Piece<br>${sagaTitle} Saga`;
+
+    // Parcourir les données JSON pour afficher chaque arc
+    for (const arc in saga.Saga.Arcs) {
+        const arcData = saga.Saga.Arcs[arc];
+
+        // Containers
+        const arcBackground = document.createElement('section');
+        arcBackground.classList.add("arc-background");
+
+        const dataDiv = document.createElement("div");
+        dataDiv.classList.add("arc-data");
+
+        // Positioning
+        const left = document.createElement("div");
+        left.classList.add("left");
+        const center = document.createElement("div");
+        center.classList.add("center");
+        const right = document.createElement("div");
+        right.classList.add("right");
+
+        // Arc title
+        const titleDiv = document.createElement("div");
+        titleDiv.classList.add("arc-title");
+
+        const title = document.createElement("h2");
+        title.classList.add("arc-title-text");
+        title.textContent += arcData.Nom;
+
+        const title2 = document.createElement("h2");
+        title2.classList.add("arc-title-text");
+        title2.classList.add("second-text");
+        title2.textContent += arcData.Nom;
+
+        // Arc protagonists
+        const equipage = document.createElement("p");
+        equipage.textContent = "Membres de l'équipage :";
+        const membresAllies = document.createElement("ul");
+        arcData.Membres_Chapeau_de_Paille.forEach(membre => {
+            const nomMembre = document.createElement("li");
+            nomMembre.textContent = membre;
+            membresAllies.appendChild(nomMembre);
+        })
+
+        // Arc antagonists
+        const ennemis = document.createElement("p");
+        ennemis.textContent = "Personnes affrontées :";
+        const membresEnnemis = document.createElement("ul");
+        arcData.Personnes_affrontees.forEach(membre => {
+            const nomMembre = document.createElement("li");
+            nomMembre.textContent = membre;
+            membresEnnemis.appendChild(nomMembre);
+        })
+
+        // Append to containers
+        titleDiv.appendChild(title);
+        titleDiv.appendChild(title2);
+
+        equipage.appendChild(membresAllies);
+        left.appendChild(equipage);
+
+        center.appendChild(document.createElement("h3"));
+
+        ennemis.appendChild(membresEnnemis);
+        right.appendChild(ennemis);
+
+        arcBackground.appendChild(titleDiv);
+        arcBackground.appendChild(left);
+        arcBackground.appendChild(center);
+        arcBackground.appendChild(right);
+
+        conteneur.appendChild(arcBackground);
+
+    }
+}
+
+const toggleSection = (hash) => {
+    document.querySelector(`section.active`)?.classList.remove('active')
+    document.querySelector(`${hash}-section`)?.classList.add('active')
+}
+
 // Event listeners
 document.addEventListener("click", e => {
     switch (e.target.classList[0]) {
@@ -72,7 +157,8 @@ document.addEventListener("click", e => {
                 .map(word => word.charAt(0).toUpperCase() + word.slice(1)) // Met la première lettre en majuscule pour chaque mot
                 .join('_'); // Réassemble les mots avec des tirets
 
-            window.location.href = arcPage + `?name=Arc_${formattedTitle}`;
+            toggleSection("saga")
+            afficherDonnees()
             break;
     }
 })

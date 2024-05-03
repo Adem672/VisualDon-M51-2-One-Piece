392b7ee82c47e987e45aa38bd9409e94ae9b8b2a
Lohann Kasper <lohann.kasper@me.com>
2024-05-03 09:39:28 +0200

    images

---
+++ src/ArcHandler/ArcGeneration.js
@@ -0,0 +1,76 @@
+import saga from "../../data/saga-data.json";
+
+const images = [...document.querySelectorAll(".img")];
+const items = [...document.querySelectorAll(".item")];
+const slider = document.querySelector(".slider");
+const arcKeys = Object.keys(saga.Saga.Arcs);
+let counter = 0;
+let sliderWidth;
+let imageWidth;
+let current = 0;
+let target = 0;
+let ease = .04;
+
+// Arc title
+items.forEach(item => {
+    const title = document.createElement("p");
+    title.classList.add("arc-title");
+    title.textContent = saga.Saga.Arcs[arcKeys[counter++]].Nom;
+    item.appendChild(title);
+})
+
+// Arc image
+images.forEach((img, index) => {
+    img.style.backgroundImage = `url(./img/arcs/${index + 1}.jpg)`;
+})
+
+// Slider + Parallax functions
+function lerp(start, end, t) {
+    return start * (1 - t) + end * t;
+}
+
+function setTransform(el, transform) {
+    el.style.transform = transform;
+}
+
+function init() {
+    sliderWidth = slider.getBoundingClientRect().width;
+    imageWidth = sliderWidth / images.length;
+    document.body.style.height = `${sliderWidth - (window.innerWidth - window.innerHeight)}px`;
+}
+
+function animate() {
+    current = parseFloat(lerp(current, target, ease)).toFixed(2);
+    target = window.scrollY;
+    setTransform(slider, `translateX(-${current}px)`);
+    animateImage();
+    requestAnimationFrame(animate);
+}
+
+function animateImage() {
+    const ratio = current / imageWidth;
+    let intersectionRatioValue;
+
+    images.forEach((image, index) => {
+        intersectionRatioValue = ratio - (index * 0.7);
+        setTransform(image, `translateX(${intersectionRatioValue * 150}px)`);
+    })
+}
+
+export function renderArcs() {
+    // Arc title
+    items.forEach(item => {
+        const title = document.createElement("p");
+        title.classList.add("arc-title");
+        title.textContent = saga.Saga.Arcs[arcKeys[counter++]].Nom;
+        item.appendChild(title);
+    })
+
+    images.forEach((img, index) => {
+        img.style.backgroundImage = `url(./img/arcs/${index + 1}.jpg)`;
+    })
+
+    init(); 
+    animate(); 
+    animateImage(); 
+}

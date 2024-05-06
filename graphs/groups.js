da5543fa6488b77ae86b8311955d12ad264a2cca
Michaël Cheneval <michael@cheneval.com>
2024-05-06 09:26:59 +0200

    Images ??

--- graphs/groups.js
+++ graphs/groups.js
@@ -3,7 +3,7 @@ import data from "../data/characters.json";
 import { findParentKey } from '../modules/jsonFunctions';
 
 const spacing = 30;
-const border = 2;
+const border = 3;
 
 export function createGroup(characters) {
     // Filter characters based on the input array
@@ -14,44 +14,66 @@ export function createGroup(characters) {
     // Create SVG container
     const svg = d3.select('body')
         .append('svg')
-        .attr('height', filteredCharacters.length * (dimensions + spacing) - spacing)
-        .attr('width', (dimensions + border))
-        .classed("characters", true);
+        .attr('height', filteredCharacters.length * (dimensions + spacing) - spacing + border * 2)
+        .attr('width', (dimensions + border));
 
-    // Define radial gradient
-    const gradient = svg.append('defs')
+    // Define allies radial gradient
+    const alliesGradient = svg.append('defs')
         .append('radialGradient')
-        .attr('id', 'radial-gradient')
+        .attr('id', 'radial-gradient-allies')
         .attr('cx', '50%')
         .attr('cy', '50%')
         .attr('r', '50%')
         .attr('fx', '50%')
         .attr('fy', '50%');
 
-    gradient.append('stop')
+    alliesGradient.append('stop')
         .attr('offset', '50%')
         .style('stop-color', '#fff');
 
-    gradient.append('stop')
+    alliesGradient.append('stop')
         .attr('offset', '100%')
-        .style('stop-color', () => {
-            const team = findParentKey(data, filteredCharacters[0]);
-            return team == "Crew" ? '#57cae7' : team == "Enemies" ? '#d24444' : '#fff';
-        });
+        .style('stop-color', '#57cae7');
+
+    // Define enemies radial gradient
+    const enemiesGradient = svg.append('defs')
+        .append('radialGradient')
+        .attr('id', 'radial-gradient-enemies')
+        .attr('cx', '50%')
+        .attr('cy', '50%')
+        .attr('r', '50%')
+        .attr('fx', '50%')
+        .attr('fy', '50%');
+
+    enemiesGradient.append('stop')
+        .attr('offset', '50%')
+        .style('stop-color', '#fff');
+
+    enemiesGradient.append('stop')
+        .attr('offset', '100%')
+        .style('stop-color', '#d24444');
 
     // Create circles and images for each character
     const charactersGroup = svg.selectAll('.character')
         .data(filteredCharacters)
         .enter()
         .append('g')
+        .classed('character', true)
         .attr('transform', (d, i) => `translate(${(dimensions + border) / 2}, ${dimensions / 2 + i * (dimensions + spacing)})`);
 
     // Add circles for gradient fill
-    charactersGroup.append('circle')
-        .attr('r', dimensions / 2)
-        .style('stroke', 'black')
-        .style('stroke-width', border)
-        .style('fill', 'url(#radial-gradient)');
+    charactersGroup.each(function (d) {
+        const group = d3.select(this);
+        group.append('circle')
+            .attr('r', dimensions / 2)
+            .style('stroke', 'black')
+            .style('stroke-width', border)
+            .style('fill', () => {
+                const team = findParentKey(data, d);
+                console.log(team);
+                return team == "Crew" ? 'url(#radial-gradient-allies)' : 'url(#radial-gradient-enemies)';
+            });
+    })
 
     // Add images to circles
     charactersGroup.each(function (d) {
@@ -69,16 +91,15 @@ export function createGroup(characters) {
             .attr('height', 1)
             .append('image')
             .attr('xlink:href', imageUrl)
-            .attr('width', dimensions - 30)
-            .attr('height', dimensions - 30)
-            .attr('x', 15)
-            .attr('y', 15);
+            .attr('width', dimensions)
+            .attr('height', dimensions);
+        // .attr('x', 15)
+        // .attr('y', 15);
 
         // Append image to the group
         group.append('circle')
             .attr('r', dimensions / 2)
             .style('fill', () => `url(#${patternId})`)
-            .classed('character', true);
     });
 
     return svg.node();

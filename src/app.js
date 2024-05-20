8a304ec62120beea9475d3a04557f82d8c8f9a4a
Lohann Kasper <lohann.kasper@me.com>
2024-05-20 15:24:27 +0200

    images and pop up

--- src/app.js
+++ src/app.js
@@ -5,11 +5,43 @@ import { scroll } from "../modules/scroll.js";
 
 let memScroll = 0;
 
-// Switch views (home / arc)
-const toggleSection = (hash) => {
-    document.querySelector(`section.active`)?.classList.remove('active');
-    document.querySelector(`#${hash}-section`)?.classList.add('active');
+const openModalButtons = document.querySelectorAll('[data-modal-target]');
+const closeModalButtons = document.querySelectorAll('[data-close-button]');
+const overlay = document.getElementById('overlay');
+
+openModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = document.querySelector(button.dataset.modalTarget);
+        openModal(modal);
+    });
+});
+
+overlay.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.modal.active');
+    modals.forEach(modal => {
+        closeModal(modal);
+    });
+});
+
+closeModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = button.closest('.modal');
+        closeModal(modal);
+    });
+});
+
+function openModal(modal) {
+    if (modal == null) return;
+    modal.classList.add('active');
+    overlay.classList.add('active');
 }
+
+function closeModal(modal) {
+    if (modal == null) return;
+    modal.classList.remove('active');
+    overlay.classList.remove('active');
+}
+
 
 document.addEventListener("click", e => {
     switch (e.target.classList[0]) {
@@ -20,7 +52,7 @@ document.addEventListener("click", e => {
             const arcTitle = e.target.parentNode.children[1].textContent;
 
             // Remplacer les espaces et les apostrophes par des tirets et capitaliser chaque mot
-            const formattedTitle = "Arc_"+arcTitle.replace(/(?:\s|')+/g, '_') // Remplace les espaces et les apostrophes par des tirets
+            const formattedTitle = "Arc_" + arcTitle.replace(/(?:\s|')+/g, '_') // Remplace les espaces et les apostrophes par des tirets
                 .toLowerCase() // Convertit tout en minuscules
                 .split('_') // Divise la chaîne en tableau de mots
                 .map(word => word.charAt(0).toUpperCase() + word.slice(1)) // Met la première lettre en majuscule pour chaque mot
@@ -41,7 +73,7 @@ document.addEventListener("click", e => {
 document.addEventListener("mousemove", e => {
     const backButton = document.querySelector("#saga-section .back");
     const screenWidth = window.innerWidth;
-    const threshold = screenWidth/32;
+    const threshold = screenWidth / 32;
     if (e.pageX >= screenWidth - threshold) {
         backButton.classList.add("show");
     } else {
